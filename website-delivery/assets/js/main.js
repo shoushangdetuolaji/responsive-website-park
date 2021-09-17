d3b428d114342d5994599cd46d4fe033950d455a
shoushangdetuolaji <tklijunhao@qq.com>
2021-09-18 04:43:05 +0900

    add About block

--- website-delivery/assets/js/main.js
+++ website-delivery/assets/js/main.js
@@ -17,6 +17,7 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
   const navMenu = document.getElementById('nav-menu')
+  // when we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
