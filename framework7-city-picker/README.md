a37b1285b200970921ea333619dce03c0d443984
v_ljhaolli(李俊豪) <v_ljhaolli@tencent.com>
2021-11-04 12:09:59 +0800

    add framework7-city-picker-demo

---
+++ framework7-city-picker/README.md
@@ -0,0 +1,7 @@
+# framework7-city-picker
+
+city-picker 使用了framework7的picker组件制作的mobile端城市级联选择器，其中picker组件可以媲美ios元素选择器。
+在`assets/data`文件中有中国所有省市的原始数组文件，以及通过原始数据文件生产的三种简易数据格式:
+- `regionsArray.json` 数组类型
+- `regionsObject.json` 对象类型
+- `regionsObject2.json` 对象类型扩展
---
+++ framework7-city-picker/assets/js/city-picker.js
@@ -0,0 +1,113 @@
+(function(window, $, undefined){
+    /**
+     * [getProvince 获取省份]
+     * @param {[Object]} regions    [省市区数据]
+     * @return {[Array]}            [省数组]
+     */
+    function getProvince(regions) {
+        return regions['provincesArr'];
+    }
+
+    /**
+     * [getCity 获取市]
+     * @param {[Object]} regions        [省市区数据]
+     * @param {[String]} provinceName   [省名]
+     * @return {[Array]}                [区数组]
+     */
+    function getCity(regions, provinceName) {
+        return regions['provinces'][provinceName]['citiesArr'];
+    }
+
+    /**
+     * [getArea 获取区]
+     * @param {[Object]} regions        [省市区数据]
+     * @param {[String]} provinceName   [省名]
+     * @param {[String]} cityName       [市名]
+     * @return {[Array]}                [区数组]
+     */
+    function getArea(regions, provinceName, cityName) {
+        return regions['provinces'][provinceName]['cities'][cityName]['areasArr'];
+    }
+
+    // 初始化 framework7
+    var myApp = new Framework7();
+
+    var province = getProvince(regions),
+        city = getCity(regions, '广东省'),
+        area = getArea(regions, '广东省', '广州市');
+
+    console.log(province);
+    console.log(city);
+    console.log(area);
+
+    // 保存 picker选择的省
+    var provinceSelect = '';
+    var pickerLocation = myApp.picker({
+        input: '#location',
+        rotateEffet: true,
+        toolbarTemplate: `
+            <div class="toolbar">
+                <div class="toolbar-inner">
+                    <div class="left">
+                        <a href="#" class="link close-picker">取消</a>
+                    </div>
+                    <div class="right">
+                        <a href="#" class="link close-picker">完成</a>
+                    </div>
+                </div>
+            </div>
+        `,
+        // 设置默认值
+        value: ['广东省', '深圳市', '南山区'],
+        cols: [
+            // 省相关
+            {
+                cssClass: 'f-s-14',
+                width: '33.33%',
+                textAlign: 'left',
+                values: province,
+                onChange: function(picker, province) {
+                    if (picker.cols[1].replaceValues) {
+                        provinceSelect = province;
+                        city = getCity(regions, province);
+                        area = getArea(regions, province, city[0]);
+                        picker.cols[1].replaceValues(city);
+                        // 区相关
+                        if (area.length !== 0) {
+                          picker.cols[2].replaceValues(area);
+                        } else {
+                          picker.cols[2].replaceValues(city);
+                        }
+                    }
+                }
+            },
+            // 市相关
+            {
+                cssClass: 'f-s-14',
+                width: '33.33%',
+                textAlign: 'center',
+                values: city,
+                // 区相关
+                onChange: function(picker, city) {
+                    if (picker.cols[2].replaceValues) {
+                        area = getArea(regions, provinceSelect, city);
+                        if (area.length !== 0) {
+                            picker.cols[2].replaceValues(area);
+                        } else {
+                            picker.cols[2].replaceValues([city]);
+                        }
+                    }
+                }
+            },
+            // 区相关
+            {
+                cssClass: 'f-s-14',
+                width: '33.33%',
+                textAlign: 'right',
+                values: area
+            }
+
+        ]
+    })
+    
+})(window, jQuery)
